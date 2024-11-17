1a0e03936a71d76da5a20a4f3113377938844d47
Vaibhav iDesai <vaibhavidesai2002@gmail.com>
2024-11-17 23:09:42 +0530

    assignment-todoApp

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import Todo from "./components/Todo"
+
+function App() {
+
+  return (
+    <div className="bg-[#34495e] grid py-4 min-h-screen">
+      <Todo/>
+    </div>
+  )
+}
+
+export default App
